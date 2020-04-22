764f5c6f01546e76df8fd6ad6e26b99f00434e8f
transgene <geneseth.sys@gmail.com>
2020-04-22 21:31:40 +0300

    fix(java9-features): add missing code
    Added code for immutable collections API that was somehow missed from the previous commit.

--- java9-features/src/main/java/net/transgene/java9/features/collections/ImmutableCollectionsTester.java
+++ java9-features/src/main/java/net/transgene/java9/features/collections/ImmutableCollectionsTester.java
@@ -1,4 +1,22 @@
 package net.transgene.java9.features.collections;
 
+import net.transgene.java9.features.varhandle.VarHandleSubject;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class ImmutableCollectionsTester {
+
+    public void testAndPrint() {
+        List<Integer> integers = List.of(1, 2, 4);
+        Set<Object> objects = Set.of("1", 2, 3L, new VarHandleSubject());
+        Map<String, Integer> stringIntegerMap = Map.ofEntries(Map.entry("key", 1), Map.entry("key2", 2));
+
+        List<Integer> intsFromOptionals = List.of(Optional.of(1), Optional.of(3), Optional.of(-1)).stream()
+                .flatMap(Optional::stream)
+                .collect(Collectors.toList());
+    }
 }
